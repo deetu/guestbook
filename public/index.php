884133dae16e1c6739672bb6f5400dc7c0834278
Bada Adedayo <deetu02@gmail.com>
2016-08-22 13:49:50 +0000

    First Commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../vendor/autoload.php';
+echo "Hello World";
+
+$array = [1, "apple", 2, "foo", "bar"];
+// var_dump($array);
+// dump($array);
+
+$database = new medoo(['database_type' => 'sqlite', 'database_file' => '../storage/database.db']);
+
+dump($database);
